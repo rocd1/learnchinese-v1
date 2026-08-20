5228c60c8859904fa3740fe240397cd63ec6e4e6
roc <d1roc1985@gmail.com>
2026-08-20 08:24:19 +0800

    angular frontend initialize

---
+++ frontend/src/app/features/guest/guest-quiz/guest-quiz.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-quiz',
+  imports: [],
+  templateUrl: './guest-quiz.html',
+  styleUrl: './guest-quiz.css',
+})
+export class GuestQuiz {}
