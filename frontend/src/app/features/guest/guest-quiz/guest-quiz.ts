a2db65123988b22404c6a988f646568ba79fd760
roc <d1roc1985@gmail.com>
2026-08-20 15:56:17 +0800

    deleted frontend with compiler issues

--- frontend/src/app/features/guest/guest-quiz/guest-quiz.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-guest-quiz',
-  imports: [],
-  templateUrl: './guest-quiz.html',
-  styleUrl: './guest-quiz.css',
-})
-export class GuestQuiz {}
